6b0a7cb730a6e393b96adb8dc68bcc6a4c9e5e20
Muhamad Taopik <mr.94t3z@gmail.com>
2024-06-22 14:24:33 +0700

    integrated with real time data

--- api/index.tsx
+++ api/index.tsx
@@ -1,5 +1,6 @@
 import { Button, Frog } from 'frog'
 import { handle } from 'frog/vercel'
+import { neynar } from 'frog/middlewares'
 import { 
   Box, 
   Column, 
@@ -12,8 +13,8 @@ import {
 
 
 // Uncomment this packages to tested on local server
-// import { devtools } from 'frog/dev';
-// import { serveStatic } from 'frog/serve-static';
+import { devtools } from 'frog/dev';
+import { serveStatic } from 'frog/serve-static';
 
 // Initialize Frog App
 export const app = new Frog({
@@ -29,7 +30,12 @@ export const app = new Frog({
     width: 1024,
   },
   browserLocation: 'https://warpcast.com/~/add-cast-action?url=https://farther-tips.vercel.app/api/frame/farther-tips',
-})
+}).use(
+  neynar({
+    apiKey: process.env.NEYNAR_API_KEY || 'NEYNAR_API_DOCS',
+    features: ['interactor', 'cast'],
+  }),
+)
 
 
 app.frame('/', async (c) => {
@@ -48,26 +54,40 @@ app.frame('/', async (c) => {
 app.castAction(
   '/farther-tips',
   (c) => {
-    // const castId = JSON.stringify(c.actionData.castId);
+    const castId = JSON.stringify(c.actionData.castId);
 
     // Parse the message back to an object to extract fid
-    // const parsedCastId = JSON.parse(castId);
-    // const fid = parsedCastId.fid;
-    // const hash = parsedCastId.hash;
+    const parsedCastId = JSON.parse(castId);
+    const fid = parsedCastId.fid;
 
-    return c.frame({ path: '/farther-tips-action'})
+    return c.frame({ path: `/farther-tips-action/${fid}`})
   }, 
   { name: "Farther Tips ✨", icon: "moon", description: "A Farcaster Cast Action to check Farther Tip Allowance.", aboutUrl: 'https://farther.social/tips'}
 )
 
 
-app.frame('/farther-tips-action', async (c) => {
+app.frame('/farther-tips-action/:fid', async (c) => {
+  const { fid } = c.req.param();
   return c.res({
     title: 'Farther Tips Allowance ✨',
-    image: '/tips',
+    image: `/check/${fid}`,
     intents: [
-      <Button action='/farther-tips-action'> Refresh </Button>,
-      <Button action='/farther-tips-action'> Check mine </Button>,
+      <Button action={`/farther-tips-action/${fid}`}> Refresh </Button>,
+      <Button  action='/check-by-me'> Check mine </Button>,
+      <Button.Link href='https://farther.social/tips'> ✨ Tip </Button.Link>
+    ],
+  })
+})
+
+
+app.frame('/check-by-me/', async (c) => {
+  const { fid } = c.var.interactor || {}
+  return c.res({
+    title: 'Farther Tips Allowance ✨',
+    image: `/check/${fid}`,
+    intents: [
+      <Button action='/check-by-me'> Refresh </Button>,
+      <Button action='/check-by-me'> Check mine </Button>,
       <Button.Link href='https://farther.social/tips'> ✨ Tip </Button.Link>
     ],
   })
@@ -363,8 +383,432 @@ app.image('/tips', async (c) => {
 })
 
 
+app.image('/check/:fid', async (c) => {
+  // Extract fid from the request
+  const { fid } = c.req.param();
+
+  // Make the API request to fetch public tips meta
+  const responseMeta = await fetch('https://farther.social/api/v1/public.tips.meta');
+
+  // Check if the response is OK (status code 200-299)
+  if (!responseMeta.ok) {
+    throw new Error(`HTTP error! Status: ${responseMeta.status}`);
+  }
+
+  // Parse the JSON response
+  const meta = await responseMeta.json();
+
+   // Extract the specific fields from the data
+   const { tipMinimum, updatedAt, _count: { allowances } } = meta.result.data[0];
+
+  // Format updatedAt date
+  const updatedDate = new Date(updatedAt);
+
+  // Format the date
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric', month: 'short', day: '2-digit',
+    hour: '2-digit', minute: '2-digit', hour12: true
+  };
+  const formattedDate = new Intl.DateTimeFormat('en-US', options).format(updatedDate);
+
+  // Make the API request to fetch user by fid
+  const params = { fid: fid };
+  const encodedParams = encodeURIComponent(JSON.stringify(params));
+  const apiUrl = `https://farther.social/api/v1/public.user.byFid?input=${encodedParams}`;
+  const responseUser = await fetch(apiUrl);
+
+  // Check if the response is OK (status code 200-299)
+  if (!responseUser.ok) {
+    throw new Error(`HTTP error! Status: ${responseUser.status}`);
+  }
+
+  // Parse the JSON response
+  const userData = await responseUser.json();
+
+  // Extract tips and currentCycle data
+  const { totals, currentCycle } = userData.result.data.tips;
+
+  // Prepare cycle date information
+  let currentCycleDate = null;
+  if (currentCycle && currentCycle.startTime) {
+    const startTime = new Date(currentCycle.startTime);
+    const month = startTime.toLocaleString('en-US', { month: 'short' }).toUpperCase();
+    const day = startTime.getDate();
+    currentCycleDate = `${month} ${day}`;
+  }
+
+  // Prepare null cycle date information from updatedAt if currentCycle is null
+  let nullCycleDate = formattedDate;
+  if (!currentCycle || !currentCycle.startTime) {
+    const nullStartDate = new Date(updatedDate);
+    const nullMonth = nullStartDate.toLocaleString('en-US', { month: 'short' }).toUpperCase();
+    const nullDay = nullStartDate.getDate();
+    nullCycleDate = `${nullMonth} ${nullDay}`;
+  }
+
+  // Extract totals and currentCycle values
+  const total_given = totals.givenCount || 0;
+  const amount_given = totals.givenAmount || 0;
+  const total_received = totals.receivedCount || 0;
+  const amount_received = totals.receivedAmount || 0;
+
+  const allowance = currentCycle ? currentCycle.allowance || 0 : 0;
+  const remaining_allowance = currentCycle ? currentCycle.remainingAllowance || 0 : 0;
+  const current_total_given = currentCycle ? currentCycle.givenCount || 0 : 0;
+  const current_amount_given = currentCycle ? currentCycle.givenAmount || 0 : 0;
+  const currect_total_received = currentCycle ? currentCycle.receivedCount || 0 : 0;
+  const currect_amount_received = currentCycle ? currentCycle.receivedAmount || 0 : 0;
+
+  return c.res({
+    headers: {
+      'cache-control': 'no-store, no-cache, must-revalidate, proxy-revalidate max-age=0, s-maxage=0',
+    },
+    image: (
+      <Box
+        grow
+        flexDirection="column"
+        alignHorizontal="center"
+        backgroundColor="bg"
+        padding="48"
+        height="100%"
+        gap="4"
+      >
+        <Box 
+          borderColor="white"
+          height="64"
+          width="100%"
+        >
+          <Box grow flexDirection="row" gap="8">
+            <Column 
+              width="2/4" 
+              padding="12" 
+              paddingLeft="0" 
+              flexDirection="row" 
+              alignHorizontal="left"
+            >
+              <Divider direction="vertical" color="yellow" />
+              <Divider direction="vertical" color="yellow" />
+              <Spacer size="10" />
+              <Text color="white" weight="600" align="center" size="32">
+                Farther Tips
+              </Text>
+            </Column>
+          </Box>
+        </Box>
+
+        <Spacer size="16" />
+
+        <Box 
+          alignHorizontal="left"
+          alignVertical="center"
+          paddingLeft="0"
+          height="128"
+          width="100%"
+        >
+          <Text color="lightGrey" weight="400" align="left" size="14">
+             CYCLE START TIME
+          </Text>
+
+          <Spacer size="12" />
+
+          <Text color="white" weight="400" align="left" size="20">
+             {formattedDate}
+          </Text>
+
+          <Spacer size="14" />
+         
+          <Box 
+            flexDirection="column" 
+            alignHorizontal="left" 
+            alignVertical="center"
+          >
+            <Box 
+              flexDirection="row" 
+              alignHorizontal="left" 
+              alignVertical="center"
+            >
+              <Text color="grey" weight="400" align="left" size="16">
+                Tip minimum:
+              </Text>
+              <Spacer size="28" />
+              <Text color="white" align="right" size="16">
+                {tipMinimum.toLocaleString()} ✨
+              </Text>
+              <Spacer size="6" />
+              <Icon name="info" color="white" size="12" />
+            </Box>
+
+            <Box 
+              flexDirection="row" 
+              alignHorizontal="left" 
+              alignVertical="center"
+            >
+              <Text color="grey" weight="400" align="left" size="16">
+                Eligible tippers:
+              </Text>
+              <Spacer size="14" />
+              <Text color="white" align="right" size="16">
+                {allowances.toLocaleString()}
+              </Text>
+              <Spacer size="6" />
+              <Icon name="info" color="white" size="12" />
+            </Box>
+          </Box>
+        </Box>
+
+        <Spacer size="16" />
+
+        <Box 
+          alignHorizontal="left"
+          alignVertical="center"
+          paddingLeft="0"
+          height="18"
+          width="100%"
+        >
+          <Text color="lightGrey" weight="400" align="left" size="14">
+            {currentCycleDate ? `${currentCycleDate} CYCLE` : `${nullCycleDate} CYCLE`}
+          </Text>
+        </Box>
+
+        <Box 
+          borderStyle="solid"
+          borderWidth="1"
+          borderRadius="8"
+          padding="18"
+          paddingLeft="24"
+          borderColor="lightGrey"
+          height="128"
+          width="100%"
+        >
+          <Box grow flexDirection="row" gap="2">
+            <Box flex="2">
+              <Box 
+                flexDirection="column" 
+                alignHorizontal="left" 
+                alignVertical="center"
+              >
+                <Box 
+                  flexDirection="row" 
+                  alignHorizontal="left" 
+                  alignVertical="center"
+                >
+                  <Text color="grey" weight="600" align="left" size="16">
+                    Allowance
+                  </Text>
+                  <Spacer size="10" />
+                  {allowance <= 0 ? (
+                  <Text color="white" align="right" size="16">
+                    0 ✨
+                  </Text>
+                  ) : (
+                  <Text color="white" align="right" size="16">
+                    {allowance.toLocaleString()} ✨
+                  </Text>
+                  )}
+                </Box>
+
+                <Box 
+                  flexDirection="row" 
+                  alignHorizontal="left" 
+                  alignVertical="center"
+                >
+                  <Text color="grey" weight="400" align="left" size="16">
+                    Given
+                  </Text>
+                  <Spacer size="48" />
+                  {current_amount_given <= 0 ? (
+                  <Text color="white" align="right" size="16">
+                    0 ✨
+                  </Text>
+                  ) : (
+                  <Text color="white" align="right" size="16">
+                    {current_amount_given.toLocaleString()} ✨
+                  </Text>
+                  )}
+                  <Spacer size="6" />
+                  {current_amount_given <= 0 ? (
+                  <Text color="lightGrey" weight="400" align="right" size="16">
+                    (0 tips)
+                  </Text>
+                  ) : (
+                  <Text color="lightGrey" weight="400" align="right" size="16">
+                    ({current_total_given} tips)
+                  </Text>
+                  )}
+                </Box>
+
+                <Box 
+                  flexDirection="row" 
+                  alignHorizontal="left" 
+                  alignVertical="center"
+                >
+                  <Text color="grey" weight="400" align="left" size="16">
+                    Remaining
+                  </Text>
+                  <Spacer size="14" />
+                  {remaining_allowance <= 0 ? (
+                  <Text color="white" align="right" size="16">
+                    0 ✨
+                  </Text>
+                  ) : (
+                  <Text color="white" align="right" size="16">
+                    {remaining_allowance.toLocaleString()} ✨
+                  </Text>
+                  )}
+                </Box>
+              </Box>
+            </Box>
+
+            <Box flex="1">
+              <Box 
+                flexDirection="column" 
+                alignHorizontal="right" 
+                alignVertical="center"
+              >
+                <Box 
+                  flexDirection="row" 
+                  alignHorizontal="right" 
+                  alignVertical="center"
+                >
+                  <Text color="grey" weight="600" align="right" size="16">
+                    Received
+                  </Text>
+                  <Spacer size="10" />
+                  {currect_amount_received <= 0 ? (
+                  <Text color="white" align="right" size="16">
+                    0 ✨
+                  </Text>
+                  ) : (
+                  <Text color="white" align="right" size="16">
+                    {currect_amount_received.toLocaleString()} ✨
+                  </Text>
+                  )}
+                  <Spacer size="6" />
+                  {currect_total_received <= 0 ? (
+                  <Text color="lightGrey" weight="400" align="right" size="16">
+                    (0 tips)
+                  </Text>
+                  ) : (
+                  <Text color="lightGrey" weight="400" align="right" size="16">
+                    ({currect_total_received} tips)
+                  </Text>
+                  )}
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+
+        <Spacer size="16" />
+
+        <Box 
+          alignHorizontal="left"
+          alignVertical="center"
+          paddingLeft="0"
+          height="18"
+          width="100%"
+        >
+          <Text color="lightGrey" weight="400" align="left" size="14">
+            TOTALS
+          </Text>
+        </Box>
+
+        <Box 
+          borderStyle="solid"
+          borderWidth="1"
+          borderRadius="8"
+          padding="20"
+          paddingLeft="24"
+          borderColor="lightGrey"
+          height="60"
+          width="100%"
+        >
+          <Box grow flexDirection="row" gap="2">
+            <Box flex="2">
+              <Box 
+                flexDirection="column" 
+                alignHorizontal="left" 
+                alignVertical="center"
+              >
+                <Box 
+                  flexDirection="row" 
+                  alignHorizontal="left" 
+                  alignVertical="center"
+                >
+                  <Text color="grey" weight="600" align="left" size="16">
+                    Given
+                  </Text>
+                  <Spacer size="10" />
+                  {amount_given <= 0 ? (
+                  <Text color="white" align="right" size="16">
+                    0 ✨
+                  </Text>
+                  ) : (
+                  <Text color="white" align="right" size="16">
+                    {amount_given.toLocaleString()} ✨
+                  </Text>
+                  )}
+                  <Spacer size="6" />
+                  {total_given <= 0 ? (
+                  <Text color="lightGrey" weight="400" align="right" size="16">
+                    (0 tips)
+                  </Text>
+                  ) : (
+                    <Text color="lightGrey" weight="400" align="right" size="16">
+                    ({total_given.toLocaleString()} tips)
+                  </Text>
+                  )}
+                </Box>
+              </Box>
+            </Box>
+
+            <Box flex="1">
+              <Box 
+                flexDirection="column" 
+                alignHorizontal="right" 
+                alignVertical="center"
+              >
+                <Box 
+                  flexDirection="row" 
+                  alignHorizontal="right" 
+                  alignVertical="center"
+                >
+                  <Text color="grey" weight="600" align="right" size="16">
+                    Received
+                  </Text>
+                  <Spacer size="10" />
+                  {amount_received <= 0 ? (
+                  <Text color="white" align="right" size="16">
+                    0 ✨
+                  </Text>
+                  ) : (
+                  <Text color="white" align="right" size="16">
+                    {amount_received.toLocaleString()} ✨
+                  </Text>
+                  )}
+                  <Spacer size="6" />
+                  {total_received <= 0 ? (
+                  <Text color="lightGrey" weight="400" align="right" size="16">
+                    (0 tips)
+                  </Text>
+                  ) : (
+                    <Text color="lightGrey" weight="400" align="right" size="16">
+                    ({total_received.toLocaleString()} tips)
+                  </Text>
+                  )}
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    ),
+  })
+})
+
+
 // Uncomment for local server testing
-// devtools(app, { serveStatic });
+devtools(app, { serveStatic });
 
 export const GET = handle(app)
 export const POST = handle(app)
